af47565830aeee106860d6adabb9601d113db3e1
ChoYeoWon1 <yw280yw@gmail.com>
2024-09-23 10:46:10 +0900

    씬 빌드 순서 편집 및 불합격 다시하기

---
+++ Assets/YW/Scripts/RetryButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryButton : MonoBehaviour
+{
+    
+    
+    public void Retry()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    
+}
